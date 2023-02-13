b2463149e7040c938ec0432cf9f6ff33daa2dbf2
Kasib <kasibabdullah8@gmail.com>
2023-02-12 19:59:15 -0500

    used css module for styling components, made color scheme

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,9 +1,10 @@
-
+import NavBarCSS from './NavBar.module.css'
 const NavBar = () => {
 
   return (
     <div>
       <h2>NavBar element</h2>
+      <button className={NavBarCSS.button}>Click Here</button>
     </div>
   )
 }
