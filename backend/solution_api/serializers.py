0256050aaac0d12892246a413c06167223277088
Kasib <kasibabdullah8@gmail.com>
2023-02-17 19:32:13 -0500

    serializer for solution gives more data with serializer method

--- backend/solution_api/serializers.py
+++ backend/solution_api/serializers.py
@@ -1,12 +1,37 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import Solution, Comment, Like
+from django.contrib.auth.models import User
 
+# class WineSerializer(serializers.Serializer):
+#     wine_name = serializers.CharField(max_length=120)
+#     price = serializers.CharField()
+#     varietal = serializers.CharField()
+#     id = serializers.IntegerField()
+#     created_at = serializers.DateTimeField()
 
 class SolutionSerializer(ModelSerializer):
+    # define the new serializer property with method field
+    measurements = serializers.SerializerMethodField()
+    creator_name = serializers.SerializerMethodField()
+
+    # make sure to add the new property to fields
     class Meta:
         model = Solution
-        fields = ["title", "instructions", "source_conc", "source_vol", "final_conc", "final_vol"]
+        fields = ["title", "creator_name", "created_at", "measurements", "instructions", "modified"]
+
+    # define how to get the value
+    def get_measurements(self, obj):
+        return {
+            "source_conc": obj.source_conc,
+            "source_vol": obj.source_vol,
+            "final_conc": obj.final_conc,
+            "final_vol": obj.final_vol
+        }
+
+    def get_creator_name(self, obj):
+        creator = obj.creator
+        return creator.username
 
     def create(self, validated_data):
         return Solution.objects.create(**validated_data)
