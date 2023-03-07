bdc9e6ccf743865585a76626a80a5b5407bd7e9a
Kasib <kasibabdullah8@gmail.com>
2023-03-06 20:39:01 -0500

    changed homepage blurb

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,11 +11,20 @@ const HomePage = () => {
 
   return (
       <div className='general-box'>
-          <h2>The HomePage</h2>
+          <h2>Welcome to the Solution Sidekick</h2>
+          <br />
           <Link to='/create_solution'><button className='submitBtn'>Let's Make a Solution</button></Link>
-          <p>I want the homepage to consist of div with formula maker</p>
-          <p>Users should be taken directly to what they need</p>
-          <p>Anyone can use the calculator, but only authenticated users can save their solutions</p>
+          <div className='homepage-blurb'>
+            <br />
+            <p>Solution Sidekick was designed to optimize your work flow.</p>
+            <br />
+            <p>Did you lose your notebook that held all of your handy dilutions? Look no further.</p>
+            <br />
+            <p>Solution Sidekick will make your calculations and allow you to save them for later use.</p>
+            <br />
+            <p>Also, checkout what your colleagues have been up to!</p>
+          </div>
+
     
       </div>
   )
