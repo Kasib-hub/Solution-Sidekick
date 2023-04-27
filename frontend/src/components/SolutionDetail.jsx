8a000ce8ff6d70e2c4a5f43806c8142396708931
Kasib <kasibabdullah8@gmail.com>
2023-04-27 16:50:22 -0400

    removed unused imports

--- frontend/src/components/SolutionDetail.jsx
+++ frontend/src/components/SolutionDetail.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import {  getWikiArticle } from '../api/SolutionAPI'
 import { Link } from 'react-router-dom'
 import './SolutionDetail.css'
