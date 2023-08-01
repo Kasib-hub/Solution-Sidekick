b272f3dbe47d5505a52078dd0248a293b232ca0e
Kasib <kasibabdullah8@gmail.com>
2023-08-01 02:34:51 -0400

    cleaned form and added alert

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -9,19 +9,25 @@ const LoginPage = () => {
 
 
   return  (
-    <div className="general-box">
+    <>
+    
+      <div className="general-box">
+      {
+      error && <Alert key="danger" variant="danger">{error.detail}</Alert>
+      }
       <h3>Login to Save Solution!</h3>
       <form onSubmit={loginUser}>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" placeholder="Enter Username" />
+        <input type="text" name="username" placeholder="Enter Username" required/>
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" placeholder="Enter password" /><br />
+        <input type="password" name="password" placeholder="Enter password" required/><br />
         <button type="submit" className="submitBtn">Login</button>
       </form>
-      {
-      error && <Alert key="danger" variant="danger">{error}</Alert>
-      }
-    </div>
+
+      </div>
+
+    </>
+
 
   )
 }
